1f0fa4315503834e2b57d2adac98cc7d43c14cc7
AdarshPawar29 <Adarsh.11601516@lpu.com>
2021-10-05 16:51:42 +0530

    POC-stoplight setup

---
+++ poc-stoplight/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { StoplightProject } from "@stoplight/elements-dev-portal";
+import "@stoplight/elements-dev-portal/styles.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <StoplightProject projectId="cHJqOjgzMTk3" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
